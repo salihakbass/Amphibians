0651ecc2cea8905ee8034e7121f1885b3b7d26f9
Salih Akbas <salihhakbass@gmail.com>
2025-01-10 22:35:32 +0300

    Serializable annotation added

---
+++ app/src/main/java/com/salihakbas/amphibians/data/model/Amphibians.kt
@@ -0,0 +1,13 @@
+package com.salihakbas.amphibians.data.model
+
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class Amphibians(
+    val name: String,
+    val type: String,
+    val description: String,
+    @SerialName(value = "img_src")
+    val imgSrc: String
+)
