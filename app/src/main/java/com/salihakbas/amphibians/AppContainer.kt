986776781500bcbee9e181364d10ef0ab3704f1e
Salih Akbas <salihhakbass@gmail.com>
2025-01-11 01:59:21 +0300

    implemented retrofit builder

---
+++ app/src/main/java/com/salihakbas/amphibians/AppContainer.kt
@@ -0,0 +1,30 @@
+package com.salihakbas.amphibians
+
+import com.jakewharton.retrofit2.converter.kotlinx.serialization.asConverterFactory
+import com.salihakbas.amphibians.data.remote.AmphibiansApiService
+import com.salihakbas.amphibians.data.repository.AmphibiansRepository
+import com.salihakbas.amphibians.data.repository.NetworkAmphibiansRepository
+import kotlinx.serialization.json.Json
+import okhttp3.MediaType.Companion.toMediaType
+import retrofit2.Retrofit
+
+interface AppContainer {
+    val amphibiansRepository: AmphibiansRepository
+}
+
+class DefaultAppContainer : AppContainer {
+    private val baseUrl = "https://android-kotlin-fun-mars-server.appspot.com"
+
+    private val retrofit = Retrofit.Builder()
+        .addConverterFactory(Json.asConverterFactory("application/json".toMediaType()))
+        .baseUrl(baseUrl)
+        .build()
+
+    private val retrofitService: AmphibiansApiService by lazy {
+        retrofit.create(AmphibiansApiService::class.java)
+    }
+
+    override val amphibiansRepository: AmphibiansRepository by lazy {
+        NetworkAmphibiansRepository(retrofitService)
+    }
+}
