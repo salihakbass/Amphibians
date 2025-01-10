b84433d5f4c6325fcdc27fce1f3130cdadace042
Salih Akbas <salihhakbass@gmail.com>
2025-01-10 22:35:57 +0300

    TopBar created

---
+++ app/src/main/java/com/salihakbas/amphibians/ui/AmphibiansApp.kt
@@ -0,0 +1,55 @@
+package com.salihakbas.amphibians.ui
+
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.material3.TopAppBarScrollBehavior
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.input.nestedscroll.nestedScroll
+import androidx.compose.ui.res.stringResource
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.salihakbas.amphibians.R
+import com.salihakbas.amphibians.ui.home.HomeScreen
+import com.salihakbas.amphibians.ui.home.HomeViewModel
+
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun AmphibiansApp() {
+    val scrollBehavior = TopAppBarDefaults.enterAlwaysScrollBehavior()
+    Scaffold(
+        modifier = Modifier.nestedScroll(scrollBehavior.nestedScrollConnection),
+        topBar = { AmphibiansTopAppBar(scrollBehavior = scrollBehavior) }
+    ) {
+        Surface(
+            modifier = Modifier.fillMaxSize()
+        ) {
+            val homeViewModel: HomeViewModel = viewModel(factory = HomeViewModel.Factory)
+            HomeScreen(
+                uiState = homeViewModel.uiState,
+                contentPadding = it,
+            )
+        }
+    }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun AmphibiansTopAppBar(scrollBehavior: TopAppBarScrollBehavior, modifier: Modifier = Modifier) {
+    CenterAlignedTopAppBar(
+        scrollBehavior = scrollBehavior,
+        title = {
+            Text(
+                text = stringResource(R.string.app_name),
+                style = MaterialTheme.typography.headlineSmall,
+            )
+        },
+        modifier = modifier
+    )
+}
